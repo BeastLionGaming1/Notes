b22b9da087153b71c6d03904f2634c16b9c8320d
BeastLionGaming1 <beaststudio42@gmail.com>
2026-06-21 21:18:08 +0300

    Third Commit

--- backend/src/router/note.router.js
+++ backend/src/router/note.router.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/create", createNote);
 router.get("/notes", getAllNotes);
 router.get("/note/:id", getNote);
-router.delete("note/:id", deleteNote);
+router.delete("/note/:id", deleteNote);
 
 export default router;
